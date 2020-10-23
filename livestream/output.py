56c9da61c3c338c8bb167d91cfa16f2699d17b3b
Selvan Ramsamy <selvanramsamy1@gmail.com>
2020-10-23 17:21:14 +0800

    update main.py

---
+++ livestream/output.py
@@ -0,0 +1,4 @@
+from input import create_medialive_input
+
+class input_creation:
+    
--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-
-import livestream.output
-import livestream.input
+from livestream.input import create_medialive_input
 
 input_name = "BD-Webinar"
 stream_key = "bangladesh-A"
 
-
-
 def main():
-	input_name
-	stream_key
-
+	input_properties = create_medialive_input(input_name, stream_key)
+	
 
 if __name__ == '__main__':
     main()
